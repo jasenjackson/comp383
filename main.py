0edfccdcf4bf8bfc3d7d2a694227501869eed604
Jasen Jackson <jasenjackson97@gmail.com>
2019-02-27 10:06:34 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+'''
+Title: HCMV Mini-project for Comp 383 (main.py)
+Author: Jasen M. Jackson, '19
+Date: 2/20/19-
+
+This scripts contains the main workflow for the Comp383 mini-projectself.
+'''
+from miniProjectDefs import *
+import sys, getopt
+import os
+import argparse
+
+## bowtie step: make sure reads map to the genome. remove those that don't
+def bt(genome, genome_name, in_files, samples, log_message):
+    print("\n## Downloading transcriptomes...")
+    sra_downloader(in_files[0],in_files[1])
+    print("\n## Creating a genome index from "+ genome)
+    bowtie_build(genome, genome_name)
+    print("\n## Mapping reads to genome index with Bowtie2...")
+    log_message += bowtie_align(genome_name,in_files[0],samples[0]) + "\n"
+    log_message += bowtie_align(genome_name,in_files[1],samples[1]) + "\n"
+    print(log_message)
+    return log_message
+
+## tophat step: compare how expression is different between two samples (requires bt step)
+def th(genome, genome_name, in_files, samples, log_message):
+    print("\n## Mapping reads to genome index with TopHat...")
+    tophat_align(genome_name,in_files[0],samples[0])
+    tophat_align(genome_name, in_files[1], samples[1])
+    print("\n## Computing change in gene expression between 2dpi & 6dpi..")
+    make_merged_transcriptomes(genome, samples[0], samples[1])
+    run_cuffdiff(genome, samples[0], samples[1])
+    log_message += filter_genes("cds.diff", 12, "yes")
+    print(log_message)
+    return log_message
+
+## spades step: which publically available strain is most similar to the patient's sample? (requires bt step)
+def sp(genome, genome_name, in_files, samples):
+    print("Assembling Bowtie2 reads in to: /hcmv_assembly/ ...")
+    assembly_name = genome_name+"_assembly"
+    fq_1_1 = in_files[0]+".mapped.1.fastq"; fq_1_2 = in_files[0]+".mapped.2.fastq"
+    fq_2_1 = in_files[1]+".mapped.1.fastq"; fq_2_2 = in_files[1]+".mapped.2.fastq"
+    spades_assemble(assembly_name, fq_1_1, fq_1_2, fq_2_1, fq_2_2)
+    num_contigs, bp = filter_contigs(assembly_name, 1000)
+    concatenate_fasta("contigs.1000.fasta", 50)
+    log_message = ("There are "+str(num_contigs)+" contigs >1000 in the assembly.\n")
+    log_message+= ("There are "+str(bp)+"bp in the assembly.\n")
+    log_file.write(log_message); print(log_message)
+    log_message = blastn_nr("contigs.1000.concatenated.fasta", 10)
+    log_file.write(log_message)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mode', required = True, type=str)
+    parser.add_argument('-g', '--genome', required = True, type=str)
+    parser.add_argument('-i', '--input', required = True, type=str)
+    parser.add_argument('-s', '--samples', type=str)
+    args = parser.parse_args()
+    samples_flag = (args.samples is not None)
+
+    print("\n---User Preferences-----")
+    print("Mode: "+args.mode) ## full, BT, BTandTH,BTandSP
+    print("Genome (fasta file): "+args.genome)
+    print("Input files: "+args.input)
+    if samples_flag: print("Samples: "+args.samples)
+
+
+    ## check genome for .fasta/.fa ending, get name of genome
+    fasta_check = (args.genome[-6:] == ".fasta")
+    fa_check = (args.genome[-3:] == ".fa")
+    if not (fasta_check or fa_check):
+        print("Error: Genome file must be in the FASTA format. (.fasta/.fa)")
+        sys.exit()
+    elif fasta_check: genome = args.genome; genome_name = args.genome[:-6]
+    elif fa_check: genome = args.genome; genome_name = args.genome[:-3]
+
+    ## split up input files/sample files and error check (only takes two arguments right now)
+    in_files = args.input.split(",")
+    if len(in_files) == 1: print("Error: Must provide at least two sequence IDs."); sys.exit()
+    if len(in_files) > 2: print("Error: Must provide only two sequence IDs."); sys.exit()
+
+    ## split up sample names into list. If none provided, use SRRxxxxx
+    if samples_flag:
+        samples = args.samples.split(",")
+        if len(in_files) == 1: print("Error: Must provide at least two sample IDs."); sys.exit()
+        if len(in_files) > 2: print("Error: Must provide only two sample IDs."); sys.exit()
+    else: samples = [args.input[0], args.input[1]]
+
+    ## setting up the "output" folder
+    if not os.path.isdir("OptionB_Jasen_Jackson/"):
+        print("\n## Made OptionB_Jasen_Jackson")
+        os.system("mkdir OptionB_Jasen_Jackson")
+    if os.path.isfile(genome):
+        print("## Copying "+genome+" to OptionB_Jasen_Jackson/"+genome)
+        os.system("cp "+genome+" OptionB_Jasen_Jackson/"+genome)
+    print("## Moving to OptionB_Jasen_Jackson\n")
+    prev_wd = os.getcwd()
+    os.chdir("OptionB_Jasen_Jackson")
+    log_file = open("OptionB.log", "w+") ## start log file.
+
+    ## get to work!
+    log_message = ""
+    log_file.write(bt(genome, genome_name, in_files, samples, log_message)) # step 1 (bt = bowtie) // in all options
+    if args.mode == "2" or args.mode == "full":
+        log_file.write(th(genome, genome_name, in_files, samples, log_message)) # step 2 (th = tophat)
+    if args.mode == "3" or args.mode == "full":
+        log_file.write(sp("HCMV.fa", "hcmv", in_files, samples, log_message)) # step 3 (sp = spades)
+
+    ## finish up
+    print("## Finishing up...deleting unnecessary files")
+    os.system("rm "+genome); print("\t## Removed copied genome folder")
+    #os.system("rm *.sra")
+    ## remove unncessary files
+    print("")
+    os.chdir(prev_wd)
+
+    print("## All done! All generated data was stored in OptionB_Jasen_Jackson/")
---
+++ miniProjectDefs.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python
+'''
+Title: HCMV Mini-project for Comp 383 (miniProjectDefs.py)
+Author: Jasen M. Jackson, '19
+Date: 2/20/19-
+
+This scripts contains the function definitions for this project's python
+wrapper.
+'''
+import os
+from itertools import (takewhile,repeat)
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
+import csv
+
+def line_count(fi): ## TODO: better alternative?
+    f = open(fi, 'rb')
+    bufgen = takewhile(lambda x: x, (f.raw.read(1024*1024) for _ in repeat(None)))
+    seq_count = int(sum( buf.count(b'\n') for buf in bufgen )/4)
+    return seq_count
+
+## Download and split list of SRA files
+def sra_downloader(*inputs):
+    from sys import platform
+    if platform == "linux" or platform == "linux2": download_command = "wget -q"
+    if platform == "darwin": download_command = "wget " # TODO: change to CURL.. get rid of WGET dependency
+    for input in inputs:
+        if os.path.isfile(input+"_1.fastq") and os.path.isfile(input+"_2.fastq"):
+            print(input+" already downloaded. Skipping!")
+        else:
+            url = "\"ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR\""
+            url += input[3:6]+'/'+input+'/'+input+'.sra'
+            print("Downloading: "+input+" from url: "+url); os.system(download_command+url)
+            print("Splitting "+input+".sra into 2 paired FASTQ files"); os.system("fastq-dump --split-3 "+input+".sra")
+
+## Run Bowtie2 build function (i: .FASTA, o: bt2 index)
+def bowtie_build(input,out):
+    print("Building index for "+input+"...")
+    bowtie_command = 'bowtie2'
+    input_exists = os.path.isfile(input)
+    if input_exists:
+        bowtie_command += '-build --quiet -f '+input+' '+out
+        print(bowtie_command)
+    else:
+        print("Error: could not locate "+input+"")
+        return False
+    os.system(bowtie_command)
+
+## Run Bowtie2 alignment (idx: bt2 index, o: .SAM, i: paired FASTQs)
+def bowtie_align(idx,input,out):
+    r1, r2 = input+"_1.fastq", input+"_2.fastq"
+    input_exists = os.path.isfile(r1) and os.path.isfile(r2)
+    index_exists = os.path.isfile(idx+".1.bt2")
+    ## Map input to index, if no mapped fastq file exists
+    if not os.path.isfile(out+".mapped.1.fastq"):
+        print("Aligning "+input+" to "+idx+" with Bowtie2")
+        ## bowtie2 --al-conc -x <idx> -1 <m1> -2 <m2> -S
+        if input_exists and index_exists:
+            bowtie_command = "bowtie2 --al-conc "+out+".mapped.fastq -x "+idx+" -1 "+r1+" -2 "+r2+" -S "+out; print(bowtie_command)
+        elif index_exists: print("Error: could not locate paired FASTQ files: "+r1+","+r2); return False
+        elif input_exists: print("Error: could not locate index file: "+idx); return False
+        else: print("Error: could not locate any files provided."); return False
+        os.system(bowtie_command)
+        print("Done!")
+    else: print(input+" has already been mapped to "+idx+" with Bowtie2. Skipping!")
+    ## Write to log file ..
+    before = str(line_count(r1))
+    after = str(line_count(out+".mapped.1.fastq"))
+    log_message = out+" had "+before+" pairs before Bowtie2 filtering and "+after+" pairs after."
+    print(log_message)
+    return log_message
+
+## Run TopHat alignment (idx: bt2 index, o: .SAM, i: paired FASTQs)
+def tophat_align(idx,input,out):
+    print("Aligning "+input+" to "+idx+" with TopHat")
+    r1, r2 = input+"_1.fastq", input+"_2.fastq"
+    input_exists = os.path.isfile(r1) and os.path.isfile(r2)
+    index_exists = os.path.isfile(idx+".1.bt2")
+    out_dir = out+"_tophat"
+    ## Map input to index, if no mapped fastq file index_exists
+    if not os.path.isfile(out_dir+"/accepted_hits.bam"):
+        if input_exists and index_exists:
+            tophat_command = "tophat -p 2 -o "+out_dir+" "+idx+" "+r1+" "+r2
+        elif index_exists: print("Error: could not locate paired FASTQ files: "+r1+","+r2); return False
+        elif input_exists: print("Error: could not locate index file: "+idx); return False
+        else: print("Error: could not locate any files provided."); return False
+        print(tophat_command)
+        os.system(tophat_command)
+    else: print(input+" has already been mapped to "+idx+" with TopHat. Skipping!")
+
+## Run Cufflinks (accepted_hits.bam >> transcripts.gtf)
+def run_cufflinks(input):
+    print("Running Cufflinks on "+input+"...")
+    output = input+"/transcripts.gtf" ## expected output
+    if not os.path.isfile(output):
+        os.system("cufflinks -p 4 -o " +input+" "+input+"/accepted_hits.bam")
+        if os.path.isfile(output):
+            print("Transcript assembly for "+input+"was a success. Results are located here: "+output)
+            return output
+        else:
+            print("\tUh oh! Cufflinks failed. *shrug* ")
+            return False
+    else:
+        print("\tCufflinks has already assembled transcripts for "+input+" in Skipping!")
+        return output
+
+## Run Cuffmerge (transcript.gtf >> merged.gtf)
+def run_cuffmerge(genome):
+    print("Merging transcripts...")
+    output = "merged_asm/merged.gtf"
+    assemblies_exists = os.path.isfile("assemblies.txt")
+    merged_asm_exists = os.path.isfile(output)
+    if not merged_asm_exists:
+        if os.path.isfile("assemblies.txt"):
+            cuffmerge_command = "cuffmerge -s "+genome+" -p 2 assemblies.txt"
+            print(cuffmerge_command)
+            os.system(cuffmerge_command)
+        else: print("Could not locate assemblies.txt")
+    else: print("\tTranscripts in assemblies.txt have already been merged. Skipping!")
+
+
+## Prepare tophat files for cuffdiff
+def make_merged_transcriptomes(genome,*inputs):
+    if not os.path.isdir("merged_transcriptome"):
+        assemblies = open("assemblies.txt", "w+")
+        ## Assemble transcripts and collect in assemblies.txt ...
+        for input in inputs:
+            if os.path.isfile(input+"/accepted_hits.bam"):
+                cl_output = run_cufflinks(input)
+                assemblies.write(cl_output+"\n")
+        ## Merge files together
+        run_cuffmerge(genome)
+    else: print("Error: Could not locate merged transcript file")
+
+## Run cuffdiff (assumes pair-wise comparison)
+def run_cuffdiff(genome, t1, t2):
+    print("## Computing differences in transcript expression using Cuffdiff...")
+    output = "cuffdiff_"+t1+"_"+t2
+    if not os.path.isfile(output):
+        cuffdiff_command = "cuffdiff -p 4 "+output+" -b "+genome+" -L "+t1+","+t2+" -u merged_asm/merged.gtf"+" ./"+t1+"/accepted_hits.bam"+",./"+t2+"/accepted_hits.bam"
+        print(cuffdiff_command)
+    else: print("Cuffdiff has already compares these transcriptomes: "+output)
+
+def filter_genes(file, r, val):
+    print("Compiling list of significant genes... ")
+    with open(file,'r') as gene_file:
+        log_message = ""
+        reader = csv.reader(gene_file, delimiter='\t')
+        for row in reader:
+            if row[r] == val:
+                log_message += " ".join(str(x) for x in row)+"\n"
+    gene_file.close()
+    return log_message
+
+## Run spades (assumes "pair-wise" assembly)
+def spades_assemble(out, *inputs):
+    print("## Assembling "+str(inputs)+" using spades")
+    in1_exists = os.path.isfile(inputs[0])
+    in2_exists = os.path.isfile(inputs[2])
+    output_exists = os.path.isdir(out)
+    #if not output_exists:
+    if in1_exists and in2_exists:
+        spades_command = "spades.py --quiet -k 55,77,99,127 -t 4 --only-assembler --pe1-1 "+inputs[0]+" --pe1-2 "+inputs[1]+" --pe2-1 "+inputs[2]+" --pe2-2 "+inputs[3]+" -o "+out+"/"
+        print(spades_command)
+        os.system(spades_command)
+    else: print("Could not find input mapped fastqs")
+
+## Output filtered contigs with length > n (saves as contigs.filtered.n.fasta)
+def filter_contigs(assembly, n):
+    print("## Filtering contigs in "+assembly+" by size "+str(n))
+    in_path = assembly+"/contigs.fasta"
+    outfile = open("contigs."+str(n)+".fasta", "w+")
+    num_contigs, size = 0,0
+    for record in SeqIO.parse(in_path, "fasta"):
+        s = record.id.find("length_")
+        l = record.id[s+7:]; l = int(l[:l.find("_")])
+        if l > n:
+            outfile.write(">"+record.id+"\n"+str(record.seq)+"\n")
+            num_contigs += 1
+            size += l
+    return num_contigs, size
+
+## Concatenate FASTA into one string, adding k Ns inbetween
+def concatenate_fasta(fasta, k):
+    print("Concatenating "+fasta+" with "+str(k)+" 'N's in between")
+    fasta_exists = os.path.isfile(fasta)
+    if fasta_exists:
+        output_name = fasta[:-5]+"concatenated.fasta"
+        output_file = open(output_name, "w+")
+        output_file.write(">"+output_name+" concatenated with "+str(k)+"stretches"+"\n")
+        n_string = 'N'*k
+        for record in SeqIO.parse(fasta, "fasta"):
+            output_file.write(str(record.seq))
+            output_file.write(n_string)
+        ## remove last 50 Ns
+        output_file.seek(0); output_file.readline()
+        concatenated = output_file.readline(); output_file.seek(0)
+        output_file.readline(); output_file.write(concatenated[:-50])
+
+## Run BLASTN on FASTA file against NR database, report top n results
+def blastn_nr(query, n):
+    print("## Performing a BLAST search against the non-redundant nucleotide database using "+query+"as query")
+    fasta_string = open(query).read()
+    results_handle = NCBIWWW.qblast("blastn", "nr", fasta_string)
+    blast_records = NCBIXML.read(result_handle)
+    blast_records = list(blast_records)
+    print("Printing top BLAST hits...")
+    log_message = ""
+    for i in range(0,n):
+        log_message += "Sequence title: "+blast_records[i].title+"\n"
+        log_message += "Alignment length: "+blast_records[i].length+"\n"
+        log_message += "HSP identities"+blast_records[i].hsp.identities+"\n"
+        log_message += "HSP query: "+blast_records[i].hsp.query+"\n"
+        log_message += "HSP bits: "+blast_records[i].hsp.bits+"\n"
+        log_message += "HSP Expect scores: "+blast_records[i].hsp.expect+"\n"
+    print(log_message)
+    return(log_message)
